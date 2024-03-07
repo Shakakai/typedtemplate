ef571946b05553e936c0e4d82653ff41b5e27226
Todd Cullen <culltp1@gmail.com>
2024-03-07 13:59:30 -0800

    cleaning up package api

--- src/typedtemplate/__init__.py
+++ src/typedtemplate/__init__.py
@@ -1,2 +1,9 @@
+from .version import VERSION, version_short
 from .template import TypedTemplate
 from .engine import BaseTemplateEngine, DjangoTemplateEngine, JinjaTemplateEngine
+
+__all__ = [
+    'VERSION', 'version_short',
+    # Template
+    'TypedTemplate', 'BaseTemplateEngine', 'DjangoTemplateEngine', 'JinjaTemplateEngine',
+]
---
+++ src/typedtemplate/version.py
@@ -0,0 +1,15 @@
+"""The `version` module holds the version information for TypedTemplate."""
+from __future__ import annotations as _annotations
+
+__all__ = 'VERSION', 'version_short'
+
+VERSION = '0.1.0'
+"""The version of TypedTemplate."""
+
+
+def version_short() -> str:
+    """Return the `major.minor` part of TypedTemplate version.
+
+    It returns '2.1' if SynCom version is '2.1.1'.
+    """
+    return '.'.join(VERSION.split('.')[:2])
